29b4079060737fe7b38aa0635e22f077f01d6cdf
Caio Batista <caio_silvabatista@hotmail.com>
2021-03-11 21:10:44 -0300

    :bug:  change api base url

--- src/providers/fetchClient.js
+++ src/providers/fetchClient.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 import { getToken } from 'helpers/auth'
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL
-  baseURL: 'http://localhost:2020'
+  baseURL: process.env.REACT_APP_API_URL
 })
 
 api.interceptors.request.use(async config => {
